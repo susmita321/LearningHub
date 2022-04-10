903f247c84e561862eae99714cc7a19183625cef
Riju Das <rite2rijudas@gmail.com>
2022-04-10 22:01:42 +0530

    Admin template added by riju and sumon

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningHub.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Landing()
+        {
+            return View();
+        }
+    }
+}
